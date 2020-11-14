a477bcd6982268e73fe8edfdba5fa2cbd2b85c33
lpassamano <leighayna.josele@gmail.com>
2020-11-13 22:42:09 -0500

    Filter member only events

--- src/api/controllers/events.controller.ts
+++ src/api/controllers/events.controller.ts
@@ -4,23 +4,33 @@ class EventsController {
   constructor() {}
 
   get = (req, res) => {
-    db.query('select * from events', (err, response) => {
-      if (err) {
-        console.log(err)
-      }
+    if (req.query.member_only) {
+      db.query('select * from events where member_only = $1', [req.query.member_only], (err, response) => {
+        if (err) {
+          console.log(err)
+        }
 
-      if (req.query.keyword) {
-        const keyword = req.query.keyword.toLowerCase()
-        const filteredEvents = response.rows.filter(event => {
-          if (event.event_name.toLowerCase().includes(keyword)) {
-            return event
-          }
-        })
-        res.send(filteredEvents)
-      } else {
         res.send(response.rows)
-      }
-    })
+      })
+    } else {
+      db.query('select * from events', (err, response) => {
+        if (err) {
+          console.log(err)
+        }
+
+        if (req.query.keyword) {
+          const keyword = req.query.keyword.toLowerCase()
+          const filteredEvents = response.rows.filter(event => {
+            if (event.event_name.toLowerCase().includes(keyword)) {
+              return event
+            }
+          })
+          res.send(filteredEvents)
+        } else {
+          res.send(response.rows)
+        }
+      })
+    }
   }
 
   getCategory = (req, res) => {
